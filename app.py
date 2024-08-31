54edc6b18884f80f6e46d5a1c298dbb489bde722
tori-Air <t0311102@icloud.com>
2024-08-31 13:22:18 +0900

    地図表示変更

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import folium
 
-folium_map = folium.Map(location=[39.701437, 141.136723], zoom_start=15)
+folium_map = folium.Map(location=[39.699657, 141.156873], zoom_start=15)
 
 
 folium_map.save("folium_map.html")
