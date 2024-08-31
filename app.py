d91b68f029b158bfada358a9b284ae38000a7216
tori-Air <t0311102@icloud.com>
2024-08-31 13:26:23 +0900

    マーカーを置く

--- app.py
+++ app.py
@@ -2,5 +2,6 @@ import folium
 
 folium_map = folium.Map(location=[39.699657, 141.156873], zoom_start=15)
 
+folium_map = folium.Marker(location=[39.699657, 141.156873]).add_to(folium_map)
 
 folium_map.save("folium_map.html")
