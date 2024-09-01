5998988203c2830bde42a925e0324899aadb64d2
tori-Air <t0311102@icloud.com>
2024-09-01 11:37:39 +0900

    html格納場所変更

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import folium
 
-folium_map = folium.Map(location=[39.699657, 141.156873], zoom_start=15)
+folium_map = folium.Map(location=[39.699657, 141.156873], zoom_start=40)
+folium_map = folium.Marker(location=[39.699657, 141.156873], icon=folium.Icon(icon="home")).add_to(folium_map)
 
-folium_map = folium.Marker(location=[39.699657, 141.156873]).add_to(folium_map)
-
-folium_map.save("folium_map.html")
+folium_map.save("templates/folium_map.html")
