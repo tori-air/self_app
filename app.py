8eb004eb90312c84d4938c8ff9e1e6b86015e720
tori-Air <t0311102@icloud.com>
2024-09-02 23:02:58 +0900

    2地点指定

--- app.py
+++ app.py
@@ -1,6 +1,34 @@
 import folium
+import osmnx as ox
 
-folium_map = folium.Map(location=[39.699657, 141.156873], zoom_start=40)
-folium_map = folium.Marker(location=[39.699657, 141.156873], icon=folium.Icon(icon="home")).add_to(folium_map)
+# 経路探索対象地域を設定
+area = ["Morioka,Iwate,Japan"]
+
+# 徒歩のみでグラフを作成
+G = ox.graph_from_place(area, network_type="walk")
+
+# 出発地点と到着地点を指定
+# departure = (39.701437, 141.136723)
+# destination = (39.699657, 141.156873)
+departure_lat, departure_lon = ox.geocoder.geocode("岩手県町")
+destination_lat, destination_lon = ox.geocoder.geocode("盛岡市役所")
+
+# 2地点の近似ノードを取得する
+# 順番は経度､緯度
+dep_node = ox.nearest_nodes(G, departure_lon, departure_lat)
+des_node = ox.nearest_nodes(G, destination_lon, destination_lat)
+
+# 最短経路探索
+shortest_route = ox.shortest_path(G, dep_node, des_node)
+
+# 経路を地図にプロット
+folium_map = ox.plot_route_folium(G, shortest_route)
+
+# 地図にマーカーを追加
+folium.Marker(location=[departure_lon, departure_lat]).add_to(folium_map)
+folium.Marker(location=[destination_lon, destination_lat]).add_to(folium_map)
+
+# folium_map = folium.Map(location=[39.33333, 141.136723], zoom_start=30)
+# folium_map = folium.Marker(location=[39.33333, 141.136723], icon=folium.Icon(icon="home")).add_to(folium_map)
 
 folium_map.save("templates/folium_map.html")
