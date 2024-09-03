1b3416bbb574297ab18252093d6ef04e6fd5c4d3
tori-Air <t0311102@icloud.com>
2024-09-03 14:06:36 +0900

    purott

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ G = ox.graph_from_place(area, network_type="walk")
 # 出発地点と到着地点を指定
 # departure = (39.701437, 141.136723)
 # destination = (39.699657, 141.156873)
-departure_lat, departure_lon = ox.geocoder.geocode("岩手県町")
-destination_lat, destination_lon = ox.geocoder.geocode("盛岡市役所")
+departure_lat, departure_lon = ox.geocoder.geocode("岩手県庁")
+destination_lat, destination_lon = ox.geocoder.geocode("盛岡駅")
 
 # 2地点の近似ノードを取得する
 # 順番は経度､緯度
@@ -21,14 +21,13 @@ des_node = ox.nearest_nodes(G, destination_lon, destination_lat)
 # 最短経路探索
 shortest_route = ox.shortest_path(G, dep_node, des_node)
 
-# 経路を地図にプロット
+# # 経路を地図にプロット
 folium_map = ox.plot_route_folium(G, shortest_route)
+# folium_map = folium.Map(location=[departure_lat, departure_lon], zoom_start=30)
+# folium_map = folium.Map(location=[destination_lat, destination_lon], zoom_start=30)
 
 # 地図にマーカーを追加
-folium.Marker(location=[departure_lon, departure_lat]).add_to(folium_map)
-folium.Marker(location=[destination_lon, destination_lat]).add_to(folium_map)
-
-# folium_map = folium.Map(location=[39.33333, 141.136723], zoom_start=30)
-# folium_map = folium.Marker(location=[39.33333, 141.136723], icon=folium.Icon(icon="home")).add_to(folium_map)
+folium.Marker(location=[departure_lat, departure_lon]).add_to(folium_map)
+folium.Marker(location=[destination_lat, destination_lon]).add_to(folium_map)
 
 folium_map.save("templates/folium_map.html")
