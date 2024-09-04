30c7a5c813565964540b3ea56b2f921e3537f5db
tori-Air <t0311102@icloud.com>
2024-09-04 14:07:50 +0900

    複数経路同時表示

--- app.py
+++ app.py
@@ -8,8 +8,6 @@ area = ["Morioka,Iwate,Japan"]
 G = ox.graph_from_place(area, network_type="walk")
 
 # 出発地点と到着地点を指定
-# departure = (39.701437, 141.136723)
-# destination = (39.699657, 141.156873)
 departure_lat, departure_lon = ox.geocoder.geocode("盛岡駅")
 destination_lat, destination_lon = ox.geocoder.geocode("ベアレン醸造所")
 
@@ -21,13 +19,51 @@ des_node = ox.nearest_nodes(G, destination_lon, destination_lat)
 # 最短経路探索
 shortest_route = ox.shortest_path(G, dep_node, des_node)
 
-# # 経路を地図にプロット
-folium_map = ox.plot_route_folium(G, shortest_route)
-# folium_map = folium.Map(location=[departure_lat, departure_lon], zoom_start=30)
-# folium_map = folium.Map(location=[destination_lat, destination_lon], zoom_start=30)
 
-# 地図にマーカーを追加
+# 主要道路を避けるためエッジにカスタム重みを設定
+def custom_weight(U, v, data):
+    # 距離情報があるか確認し､ない場合はデフォルト値(1メートル)
+    length = data.get("length", 1)
+    highway = data.get("highway", "")
+    # 狭い道は低コスト
+    if highway in ["residential", "service", "unclassified", "tertiary"]:
+        return length
+    # 主要道路は高コスト
+    else:
+        return length * 10
+
+
+# カスタム重みを使って経路を計算
+backstreet_route_1 = ox.shortest_path(G, dep_node, des_node, weight=custom_weight)
+backstreet_route_2 = ox.shortest_path(G, dep_node, des_node, weight=custom_weight)
+
+
+# 経路を地図にプロット
+folium_map = ox.plot_route_folium(
+    G, shortest_route, route_map=None, popup_label="Shortest Route"
+)
+folium_map = ox.plot_route_folium(
+    G, backstreet_route_1, route_map=folium_map, route_color="green", popup_label="Backstreet Route 1"
+)
+folium_map = ox.plot_route_folium(
+    G, backstreet_route_2, route_map=folium_map, route_color="red", popup_label="Backstreet Route 2"
+)
+
+# shortest_route 地図にマーカーを追加
 folium.Marker(location=[departure_lat, departure_lon], icon=folium.Icon(icon="user")).add_to(folium_map)
 folium.Marker(location=[destination_lat, destination_lon], icon=folium.Icon(color="red", icon="map-marker")).add_to(folium_map)
+
+# backstreet_route_1 地図にマーカーを追加
+# folium.Marker(location=[departure_lat, departure_lon], icon=folium.Icon(icon="user")).add_to(folium_map_1)
+# folium.Marker(
+#     location=[destination_lat, destination_lon], icon=folium.Icon(color="red", icon="map-marker")
+# ).add_to(folium_map_1)
+
+# # backstreet_route_2 地図にマーカーを追加
+# folium.Marker(location=[departure_lat, departure_lon], icon=folium.Icon(icon="user")).add_to(folium_map_2)
+# folium.Marker(
+#     location=[destination_lat, destination_lon], icon=folium.Icon(color="red", icon="map-marker")
+# ).add_to(folium_map_2)
+
 
 folium_map.save("templates/folium_map.html")
